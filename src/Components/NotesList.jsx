a2bc55b435b68476b9d9793bc634112958431b7b
Mohammad Hasim Shaikh <hasims2001@gmail.com>
2023-11-13 14:16:05 +0530

    localstorage updated

--- src/Components/NotesList.jsx
+++ src/Components/NotesList.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react'
 import { Card } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 
--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,16 +1,18 @@
 import { ADDNOTE } from "./actionType"
 
 const init = {
-    notes : [],
+    notes : JSON.parse(localStorage.getItem('notes')) || [],
 }
 
 const reducer = (state= init, {type, payload})=>{
     switch(type){
         case ADDNOTE:
-            return {
+            const new_state = {
                 ...state,
                 notes: [...state.notes, payload].sort((a,b)=> b.timestamp - a.timestamp),
-            };
+            }
+            localStorage.setItem('notes', JSON.stringify(new_state.notes))
+            return new_state;
         default:
             return state;
     }
